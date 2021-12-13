bcaefb50f339838a0c11c753cdaab9348d0e16e6
Nikita Aleksandrovich <Besedino_33>
2021-12-13 16:18:20 +0200

    - update

--- src/controllers/payments/StripeController.js
+++ src/controllers/payments/StripeController.js
@@ -54,7 +54,7 @@ module.exports = {
      * @returns {Promise<*>}
      */
     poprey: async function(request, response) {
-        response.redirect(response.json(await servicePoprey(request.body)));
+        response.redirect(await servicePoprey(request.body));
     },
 
 }
