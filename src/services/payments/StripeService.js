3343711a0a2a03a5c8b698942c3481c0345bb90e
Nikita Aleksandrovich <Besedino_33>
2021-12-13 16:40:59 +0200

    - update

--- src/services/payments/StripeService.js
+++ src/services/payments/StripeService.js
@@ -200,6 +200,7 @@ module.exports = {
                 fail_link: fail_link
             });
             console.log(invoice.url)
+            return
             return invoice.url
         } catch (error) {
             apiErrorLog(error);
@@ -251,6 +252,8 @@ module.exports = {
                     email: order.email
                 }
             });
+            console.log(order.success_link)
+            return
             return order.success_link;
         } catch (error) {
             apiErrorLog(error);
@@ -291,7 +294,8 @@ module.exports = {
                     email: order.email
                 }
             });
-
+            console.log("CANCEL" + order.success_link)
+            return
             return order.fail_link;
         } catch (error) {
             apiErrorLog(error);
